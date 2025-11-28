98be769c86768d3987b562b6133b9ec90fa77708
Hassan <hassanmohammedbarafah836@gmail.com>
2025-11-28 13:16:24 +0300

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -287,13 +287,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
  const register = async (username: string, email: string, password: string, birthDate: string) => {
   try {
     // استدعاء API التسجيل
-const response = await authService.register({
-  username,
-  email,
-  password,
-  birthDate,
-});
-
+const response = await authService.register(username, email, password, birthDate);
     const userData = response.user || response;
     const accessToken = response.accessToken;
     const refreshToken = response.refreshToken;
