091943b26e4809483041b5133c2359c30add0fe1
Hassan <hassanmohammedbarafah836@gmail.com>
2025-11-28 12:56:30 +0300

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,7 +4,7 @@ import { useAuth } from '../context/AuthContext';
 import { useTranslation } from 'react-i18next';
 import { useLanguage } from '../context/LanguageContext';
 import { motion, AnimatePresence } from 'framer-motion';
-import api from '../services/api';
+import { authService } from '../services/auth';
 
 const Register: React.FC = () => {
   const { t } = useTranslation();
